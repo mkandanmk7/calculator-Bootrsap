d5e241a98c142c98bbf733c8af660db63f0f0238
muthu <mkandanmk7@gmail.com>
2021-08-24 08:19:07 +0530

    calculator ready

--- calculatorBoots.js
+++ calculatorBoots.js
@@ -1,21 +1,28 @@
 let ans = document.querySelector("#result");
 
 function conCat(keys) {
-  ans.value += keys;
+  ans.value += keys; //concat the two numbers
   console.log(ans.value);
 }
 
-function evaluate() {
+function result() {
+  if (ans.value == "") {
+    confirm("Enter Number First...!");
+    ans.value = 0; //reseting values
+  }
   try {
-    ans.value = eval(ans.value);
-  } catch (error) {
+    ans.value = eval(ans.value); //arithmatic operations:
+  } catch (err) {
     alert("Enter Valid Input");
+    ans.value = 0;
   }
 }
-function clear() {
-  ans.value = "";
+function del() {
+  console.log("clear");
+  ans.value = 0;
 }
 
 function backSpace() {
+  console.log("backSpace");
   ans.value = ans.value.slice(0, -1);
 }
